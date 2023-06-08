72c84a8bce1da75ed55f4a9d27a80f59098adafa
IMyself <365172043@qq.com>
2023-06-08 15:04:12 +0800

    firstCommit

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
-    url: "https://lmyself.top/v1/chat/completions",  // gpt3.5代理接口
+    url: "https://cself.lmyself.top/v1/chat/completions",  // gpt3.5代理接口
     apiKey: ""  // openAi Base64 编码 apiKey
 };  
