8f6ff74d9dc9ea929cd28cebf53c229e4790e571
IMyself <365172043@qq.com>
2023-06-08 11:20:47 +0800

    firstCommit

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+const config = {
+    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
+    url: "https://openai-proxy.365172043.workers.dev/v1/chat/completions",  // gpt3.5代理接口
+    apiKey: ""  // openAi Base64 编码 apiKey
+};  
